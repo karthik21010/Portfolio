9ec7a57bcfd95d3a905c955dbeea4ae452e6cef1
karthik21010 <116083779+karthik21010@users.noreply.github.com>
2023-11-18 08:33:57 +0530

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,43 @@
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContent.forEach(tabContents => {
+            tabContents.classList.remove("skills_active")
+        })
+
+        target.classList.add('skills_active')
+
+
+        tabs.forEach(tab => {
+            tab.classList.remove("skills_active")
+        })
+        
+        tab.classList.add('skills_active')
+    })
+})
+
+const modalViews = document.querySelectorAll('.services_modal'),
+      modelBtns = document.querySelectorAll('.services_button'),
+      modalCloses = document.querySelectorAll('.services_modal-close')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modelBtns.forEach((modelBtn, i) =>{
+    modelBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener("click", () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
